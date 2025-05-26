3c1a3d91a4cb9f27037e8e2fa5ba8bf0bdd3db84
Victor Martinez <knoopx@gmail.com>
2025-05-26 02:58:57 +0200

    feat: enhance network speed display logic to conditionally show upload and download labels

--- widgets/TopBar/Network/index.tsx
+++ widgets/TopBar/Network/index.tsx
@@ -6,19 +6,37 @@ import { format } from "../../../support/util";
 
 export default () => (
   <box>
-    {bind(networkSpeed).as(({ download, upload }) => (
-      <box
-        widthRequest={90}
-        vertical
-        valign={Gtk.Align.CENTER}
-        css={`
-          font-size: 0.6em;
-          opacity: 0.8;
-        `}
-      >
-        <label halign={Gtk.Align.END} label={format(upload) + "/s ▲"} />
-        <label halign={Gtk.Align.END} label={format(download) + "/s ▼"} />
-      </box>
-    ))}
+    {bind(networkSpeed).as(({ download, upload }) => {
+      const threshold = 1024; // 1 KB/s threshold
+      const showUpload = upload >= threshold;
+      const showDownload = download >= threshold;
+
+      return (
+        <box
+          widthRequest={90}
+          vertical
+          valign={Gtk.Align.CENTER}
+          css={`
+            font-size: 0.6em;
+            opacity: 0.8;
+          `}
+        >
+          {showUpload && (
+            <label
+              halign={Gtk.Align.END}
+              label={format(upload) + "/s ▲"}
+              css="color: @theme_selected_bg_color;"
+            />
+          )}
+          {showDownload && (
+            <label
+              halign={Gtk.Align.END}
+              label={format(download) + "/s ▼"}
+              css="color: @theme_selected_bg_color;"
+            />
+          )}
+        </box>
+      );
+    })}
   </box>
 );
