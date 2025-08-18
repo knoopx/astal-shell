048071b2b08b1f43173f0a0db712ccf1df920516
Victor Martinez <knoopx@gmail.com>
2025-08-18 21:50:31 +0200

    refactor(SysTray): ♻️ Simplify SysTray component structure
    Refactored the SysTray component to improve readability and maintainability. Removed unnecessary nested boxes and streamlined the binding of properties.
    
    - Consolidated the structure of the component.
    - Enhanced clarity by reducing nesting.
    
    No functional changes were made.

--- widgets/TopBar/SysTray/index.tsx
+++ widgets/TopBar/SysTray/index.tsx
@@ -1,42 +1,39 @@
 import Tray from "gi://AstalTray";
-import { createBinding, With, For } from "ags";
+import { createBinding, For } from "ags";
 
 export default () => {
   const tray = Tray.get_default();
   const items = createBinding(tray, "items");
 
   return (
-    <box class="SysTray">
-      <box>
-        <For each={items}>
-          {(item: any) => {
-            const tooltipMarkup = createBinding(item, "tooltipMarkup");
-            const actionGroup = createBinding(item, "actionGroup");
-            const menuModel = createBinding(item, "menuModel");
-            const gicon = createBinding(item, "gicon");
+    <box class="SysTray" spacing={8}>
+      <For each={items}>
+        {(item: any) => {
+          const tooltipMarkup = createBinding(item, "tooltipMarkup");
+          const menuModel = createBinding(item, "menuModel");
+          const gicon = createBinding(item, "gicon");
 
-            return (
-              <menubutton
-                tooltipMarkup={tooltipMarkup}
-                usePopover={false}
-                menuModel={menuModel}
+          return (
+            <menubutton
+              tooltipMarkup={tooltipMarkup}
+              usePopover={false}
+              menuModel={menuModel}
+              css={`
+                padding: 0;
+                margin: 0;
+                background: none;
+              `}
+            >
+              <icon
+                gicon={gicon}
                 css={`
-                  padding: 0;
-                  margin: 0;
-                  background: none;
+                  font-size: 18px;
                 `}
-              >
-                <icon
-                  gicon={gicon}
-                  css={`
-                    font-size: 18px;
-                  `}
-                />
-              </menubutton>
-            );
-          }}
-        </For>
-      </box>
+              />
+            </menubutton>
+          );
+        }}
+      </For>
     </box>
   );
 };
