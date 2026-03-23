3689a3f5d2a06e640345e5db8c7b132b2dfa2c4f
Victor Martinez <knoopx@gmail.com>
2026-03-23 16:34:42 +0100

    fix(volume): 🔊 use imperative signal for slider to allow user interaction

--- widgets/TopBar/QuickSettings/Volume.tsx
+++ widgets/TopBar/QuickSettings/Volume.tsx
@@ -1,20 +1,28 @@
 import Wp from "gi://AstalWp";
-import { createBinding } from "ags";
+import { createBinding, onCleanup } from "ags";
 import { Gtk } from "ags/gtk4";
+import Astal from "gi://Astal?version=4.0";
 
 export default () => {
   const speaker = Wp.get_default()!.audio.defaultSpeaker;
   const volumeIcon = createBinding(speaker, "volumeIcon");
-  const volume = createBinding(speaker, "volume");
+
+  const slider = (<slider
+    widthRequest={100}
+    value={speaker.volume}
+    onChangeValue={(_self, _scroll, value) => { speaker.volume = value }}
+  />) as Astal.Slider;
+
+  const signalId = speaker.connect("notify::volume", () => {
+    slider.value = speaker.volume;
+  });
+
+  onCleanup(() => speaker.disconnect(signalId));
 
   return (
     <box spacing={4} valign={Gtk.Align.CENTER}>
       <image iconName={volumeIcon} pixelSize={16} />
-      <slider
-        widthRequest={100}
-        onChangeValue={({ value }) => (speaker.volume = value)}
-        value={volume}
-      />
+      {slider}
     </box>
   );
 };
